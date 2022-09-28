290a5cf363cf1d260e8407b191d810114b8ce866
Clancy Thomas <clancyvthomas@gmail.com>
2022-09-27 21:05:10 -0600

    Initial Commit

---
+++ ScriptureWordCounter/ScriptureWordCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptureWordCounterApp.swift
+//  ScriptureWordCounter
+//
+//  Created by Clancy Thomas on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ScriptureWordCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
