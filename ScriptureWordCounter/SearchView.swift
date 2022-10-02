70bf8e407207cc36c204b76bb7693ab679b22760
Clancy Thomas <clancyvthomas@gmail.com>
2022-10-01 20:14:58 -0600

    SearchView.swift : Created the format and basic layout of the search view tab

--- ScriptureWordCounter/SearchView.swift
+++ ScriptureWordCounter/SearchView.swift
@@ -7,14 +7,16 @@
 
 import SwiftUI
 
+// Custom button with a simple animation
 struct CustomButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
+            .font(.title2)
+            .fontWeight(.bold)
             .padding()
-            .frame(width: 200, height: 50)
+            .frame(width: 300, height: 60)
             .background(Color(red: 179/255, green: 150/255, blue: 89/255))
             .foregroundColor(.black)
-            .fontWeight(.bold)
             .cornerRadius(12)
             .scaleEffect(configuration.isPressed ? 0.98 : 1)
             .animation(.easeIn(duration: 0.1), value: 0.98)
@@ -22,19 +24,81 @@ struct CustomButton: ButtonStyle {
 }
 
 struct SearchView: View {
+    
+    @FocusState private var focus: Bool
+    @State var wordToSearch = ""
+    @State var bookOfMormon = false
+    @State var doctrineAndCovenants = false
+    @State var pearlOfGreatPrice = false
+    @State var oldTestament = false
+    @State var newTestament = false
+    
+    
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Search Page Here")
-            Spacer()
-            Button("Search") {
-                // Search Action Here
+            VStack {
+                Spacer()
+                VStack {
+                    Text("Enter a Word")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    TextField("Required", text: $wordToSearch)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width:300)
+                        .padding(.bottom, 50)
+                        .focused($focus)
+                }
+                
+                // VStack that contains all the scripture book options
+                VStack {
+                    Text("Click on Which Books to Search")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    Toggle("Old Testament", isOn: $oldTestament)
+                        .toggleStyle(.button)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(red: 179/255, green: 150/255, blue: 89/255), lineWidth:2)
+                        )
+                        .padding(5)
+                    Toggle("New Testament", isOn: $newTestament)
+                        .toggleStyle(.button)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(red: 179/255, green: 150/255, blue: 89/255), lineWidth:2)
+                        )
+                        .padding(5)
+                    Toggle("Book of Mormon", isOn: $bookOfMormon)
+                        .toggleStyle(.button)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(red: 179/255, green: 150/255, blue: 89/255), lineWidth:2)
+                        )
+                        .padding(5)
+                    Toggle("Pearl of Great Price", isOn: $pearlOfGreatPrice)
+                        .toggleStyle(.button)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(red: 179/255, green: 150/255, blue: 89/255), lineWidth:2)
+                        )
+                        .padding(5)
+                    Toggle("Doctrine and Covenants", isOn: $doctrineAndCovenants)
+                        .toggleStyle(.button)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color(red: 179/255, green: 150/255, blue: 89/255), lineWidth:2)
+                        )
+                        .padding(5)
+                }
+                
+                Spacer()
+                Button("Search") {
+                    // Search Action Here
+                }
+                .buttonStyle(CustomButton())
+                Spacer()
             }
-            .buttonStyle(CustomButton())
-            Spacer()
-        }
-        
     }
+    
 }
 
 struct SearchView_Previews: PreviewProvider {
