365649e1f74696abbd9cd79bbe467aaf3a9ff2d6
Clancy Thomas <clancyvthomas@gmail.com>
2022-10-01 20:06:16 -0600

    ContentView.swift : Removed HomeView and renamed ContentView to BaseView

--- ScriptureWordCounter/BaseView.swift
+++ ScriptureWordCounter/BaseView.swift
@@ -7,18 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BaseView: View {
     var body: some View {
         TabView {
-            HomeView()
-                .tabItem {
-                    Label("Home", systemImage: "house")
-                }
             SearchView()
                 .tabItem {
                     Label("Search", systemImage: "magnifyingglass")
                 }
-            SearchView()
+            SavedSearchView()
                 .tabItem {
                     Label("Saved", systemImage: "bookmark")
                 }
@@ -33,6 +29,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        BaseView()
     }
 }
--- ScriptureWordCounter/ScriptureWordCounterApp.swift
+++ ScriptureWordCounter/ScriptureWordCounterApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScriptureWordCounterApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            BaseView()
         }
     }
 }
