859b6c787733ded2232f579f04b1e5a85743b6e9
Clancy Thomas <clancyvthomas@gmail.com>
2022-10-03 15:24:28 -0600

    GroupBox.swift : Added a custom group box template

---
+++ HolyWordHunt/Components/GroupBox.swift
@@ -0,0 +1,25 @@
+//
+//  GroupBox.swift
+//  ScriptureWordCounter
+//
+//  Created by Clancy Thomas on 10/3/22.
+//
+
+import SwiftUI
+
+struct GroupBoxCustom: GroupBoxStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+            configuration.label
+                .font(.title3)
+                .fontWeight(.bold)
+            configuration.content
+                .frame(width: 275)
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 8, style: .continuous)
+            .fill(Color(red: 176/255, green: 196/255, blue: 222/255)))
+    
+    }
+}
+
