572ad6625fd292b41d116b704723b9e0854d6982
Clancy Thomas <clancyvthomas@gmail.com>
2022-10-04 21:08:37 -0600

    SearchObject.swift, SavedSearchView.swift : Created a dynamic view based on saved search list

--- HolyWordHunt/ModelData/SearchObject.swift
+++ HolyWordHunt/ModelData/SearchObject.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-struct Search {
+struct Search: Identifiable {
+    var id: Int
     var word: String
     var wordCount: Int
     var totalVerseCount: Int
@@ -16,9 +17,11 @@ struct Search {
     var bookOfMormon: Bool
     var doctrineAndCovenants: Bool
     var pearlOfGreatPrice: Bool
+    var booksString: String
     
-    init(word: String, wordCount: Int, totalVerseCount: Int, oldTestament: Bool, newTestament: Bool,
+    init(id: Int, word: String, wordCount: Int, totalVerseCount: Int, oldTestament: Bool, newTestament: Bool,
          bookOfMormon: Bool, doctrineAndCovenants: Bool, pearlOfGreatPrice: Bool) {
+        self.id = id
         self.word = word
         self.wordCount = wordCount
         self.totalVerseCount = totalVerseCount
@@ -27,5 +30,32 @@ struct Search {
         self.bookOfMormon = bookOfMormon
         self.doctrineAndCovenants = doctrineAndCovenants
         self.pearlOfGreatPrice = pearlOfGreatPrice
+        self.booksString = booksToString()
+        
+    func booksToString() -> String
+    {
+        var books = ""
+        if (oldTestament) {
+            books += "OT, "
+        }
+        if (newTestament) {
+            books += "NT, "
+        }
+        if (bookOfMormon) {
+            books += "BOM, "
+        }
+        if (doctrineAndCovenants) {
+            books += "D&C, "
+        }
+        if (pearlOfGreatPrice) {
+            books += "PGP, "
+        }
+        if (books.count > 2) {
+            books.remove(at: books.index(before: books.endIndex))
+            books.remove(at: books.index(before: books.endIndex))
+        }
+        return books
+    }
+        
     }
 }
--- HolyWordHunt/Views/SavedSearchView.swift
+++ HolyWordHunt/Views/SavedSearchView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct SavedSearchView: View {
+    @StateObject var viewModel = SavedSearchModelData()
+    
+    
     var body: some View {
-        Text("Saved Search View")
+        ScrollView(showsIndicators: false) {
+            Text("")
+                .padding(.top, 50)
+            ForEach (viewModel.savedSearches) { Search in
+                SavedSearchRow(search: Search)
+                    .onLongPressGesture {
+                        viewModel.deleteSavedSearch(id: Search.id)
+                        viewModel.updateSavedSearches()
+                    }
+                    
+            }
+            
+        }
+        .overlay(NavigationBar(title: "Saved Searches"))
+        .onAppear{viewModel.updateSavedSearches()}
     }
 }
 
